bab104e66523219b6292dea4546fe00afc4d8f74
sureshreddyrayapati <resuresh0@gmail.com>
2024-02-16 09:32:13 +0530

    Add project files.

---
+++ EvenArray/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EvenArray;
+Console.WriteLine("Welcome to number System.......!");
+Console.Write("Odd Number : ");
+Number_System.OddNumber();
+Console.WriteLine();
+Console.Write("Even Number: ");
+Number_System.EvenNumber();
